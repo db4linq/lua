99665b5de6fde2872433faea9c616c6a9dbcace7
db4linq <db4linq@gmail.com>
2015-02-23 21:16:10 +0700

    check startup file

--- init.lua
+++ init.lua
@@ -1,16 +1,15 @@
-ssid = "you-ssid"
-pwd = "you-password"
-FILE = "http.lua"
+ssid = "you_ssid"
+pwd = "you_password"
+FILE = "run.lua"
 
 function file_exists(name)
-   local f=io.open(name,"r")
-   if f~=nil then io.close(f) return true else return false end
+   local f=file.open(name,"r")
+   if f~=nil then file.close(f) return true else return false end
 end
 
 if true then  --change to if true
      print("set up wifi mode")
      wifi.setmode(wifi.STATION)
-     --please config ssid and password according to settings of your wireless router.
      wifi.sta.config(ssid, pwd)
      wifi.sta.connect()
      cnt = 0
@@ -20,22 +19,20 @@ if true then  --change to if true
           cnt = cnt + 1 
          else 
           tmr.stop(1)
-          if (cnt < 20) then print("Config done, IP is "..wifi.sta.getip())
-          if (file_exists) then
-               --dofile(FILE)
-               print("Run script...")
-          else
-               print("File not found: "..FILE)
-          end
-          else print("Wifi setup time more than 20s, Please verify wifi.sta.config() function. Then re-download the file.")
+          if (cnt < 20) then 
+               print("Config done, IP is "..wifi.sta.getip())
+               if (file_exists(FILE)) then
+                    dofile(FILE)
+                    print("Run script...")
+               end
+          else 
+               print("Wifi setup time more than 20s, Please verify wifi.sta.config() function. Then re-download the file.")
           end
          end 
-      end)
+     end)
 else
      print("\n")
      print("Please edit 'init.lua' first:")
-     --print("Step 1: Modify wifi.sta.config() function in line 5 according settings of your wireless router.")
-     --print("Step 2: Change the 'if false' statement in line 1 to 'if true'.")
 end
 
 
